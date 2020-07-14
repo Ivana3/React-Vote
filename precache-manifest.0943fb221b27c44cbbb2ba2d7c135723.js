4a23770a15f4b7d570a30a6d323698a48ae9bae6
Ivana Filipovska <ivanafilipovska996@gmail.com>
2020-07-14 14:30:18 +0200

    Updates

---
+++ precache-manifest.0943fb221b27c44cbbb2ba2d7c135723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507a61aa45306fdc8c69ed8115007fb1",
+    "url": "/React-Vote/index.html"
+  },
+  {
+    "revision": "e5839827bc848ed82498",
+    "url": "/React-Vote/static/css/main.13c2163e.chunk.css"
+  },
+  {
+    "revision": "e03e48d53d119121294f",
+    "url": "/React-Vote/static/js/2.b782c7b4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Vote/static/js/2.b782c7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5839827bc848ed82498",
+    "url": "/React-Vote/static/js/main.9740dad5.chunk.js"
+  },
+  {
+    "revision": "0010c106ed69b8b98010",
+    "url": "/React-Vote/static/js/runtime-main.76c8596c.js"
+  }
+]);
